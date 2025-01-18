97e871074681a448f8dde1c86d907fb7bc6c8fe7
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-01-18 15:18:57 +0530

    Remove redundant installation of python3-venv package from Jenkins pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,10 +9,7 @@ pipeline {
         stage('Install Dependencies') {
             steps {
                 script {
-                    // Install python3-venv package if not installed
                     sh """
-                    sudo apt-get update
-                    sudo apt-get install -y python3.12-venv
                     python3 -m venv venv
                     source venv/bin/activate
                     pip install -r requirements.txt || echo "No requirements.txt found"
