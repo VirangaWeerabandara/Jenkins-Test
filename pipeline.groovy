38d10aeebef0adabaabc91cb4c22527580f653ca
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-01-18 15:23:04 +0530

    Fix activation command for Python virtual environment in Jenkins pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -27,7 +27,7 @@ pipeline {
                 script {
                     sh """
                     python3 -m venv venv
-                    source venv/bin/activate
+                    . venv/bin/activate
                     pip install -r requirements.txt || echo "No requirements.txt found"
                     """
                 }
@@ -38,7 +38,7 @@ pipeline {
             steps {
                 script {
                     sh """
-                    source venv/bin/activate
+                    . venv/bin/activate
                     python jenkins_test.py
                     """
                 }
